c32d877c42a58cfaacdeff11b5bbdb6683fedf8f
roman-horbachov <roman.horbachov@student.karazin.ua>
2024-11-21 22:46:58 +0200

    Delete caesar_cipher directory

--- caesar_cipher/test_caesar_cipher.rb
+++
@@ -1,8 +0,0 @@
-require 'caesar_cipher'
-
-cipher = CaesarCipher::Cipher.new(3)
-encoded = cipher.encode("Hello, World!")
-puts "Encoded: #{encoded}" # Очікується: Khoor, Zruog!
-
-decoded = cipher.decode(encoded)
-puts "Decoded: #{decoded}" # Очікується: Hello, World!
